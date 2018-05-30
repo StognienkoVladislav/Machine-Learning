607a47e4db5011c774946dd698cfd1a39065078d
Vladislav Stognienko <stognienkovv@gmail.com>
2018-05-30 15:07:02 +0300

    python_spot

---
+++ Pythonspot/K_nearest/iris_sample.py
@@ -0,0 +1,44 @@
+
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import neighbors, datasets
+from matplotlib.colors import ListedColormap
+
+n_neighbors = 6
+
+# import some data to play with
+iris = datasets.load_iris()
+
+# take the first two features
+X = iris.data[:, :2]
+y = iris.target
+h = .02     # step size in the mesh
+
+# Create color maps
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA','#00FFAA'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00','#00AAFF'])
+
+# we create an instance of Neighbours Classifier and fit the data.
+clf = neighbors.KNeighborsClassifier(n_neighbors, weights='distance')
+clf.fit(X, y)
+
+# Calculate min, max and limits
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+# predict class using data and kNN classifier
+Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.figure()
+plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold)
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+plt.title('3-Class classification (k = %i)' % (n_neighbors))
+plt.show()
+
